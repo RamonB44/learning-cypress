4031be42eb5ea2e0c9bbb0473e87a34dab574047
LordHunter <huntertmgon@gmail.com>
2023-09-04 18:28:06 -0500

    upload test-example

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
   e2e: {
+    baseUrl: "https://petstore.swagger.io",
+    // "specPattern": "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
