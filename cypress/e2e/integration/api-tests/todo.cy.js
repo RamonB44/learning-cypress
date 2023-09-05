b0c90421ae3c01d2956a882ead61591993cf0c5c
LordHunter <huntertmgon@gmail.com>
2023-09-04 21:38:50 -0500

    ejemplo rapido

--- cypress/e2e/integration/api-tests/todo.cy.js
+++ cypress/e2e/integration/api-tests/todo.cy.js
@@ -1,5 +1,46 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('User CRUD feature', () => {
+  beforeEach(() => {
+    cy.visit('https://petstore.swagger.io/');
+  });
+
+  it('should create a new user', () => {
+    cy.request('POST', '/v2/user', {
+      username: 'ramonb44',
+      firstname: 'ramon',
+      lastname: 'aguado',
+      email: 'aguado.soft@gmail.com',
+      password: '#NewPass',
+      phone: '918285752'
+    }).then(response => {
+      expect(response.status).to.equal(200);
+      expect(response.body.code).to.equal(200);
+      expect(response.body.type).to.equal('unknown');
+      // expect(response.body.message).to.equal('EXCLUDE');
+    });
+  });
+
+  it('should get user data by username', () => {
+    cy.request('/v2/user/ramonb44').then(response => {
+      expect(response.status).to.equal(200);
+    });
+  });
+
+  it('should update user firstname and email', () => {
+    cy.request('PUT', '/v2/user/ramonb44', {
+      username: 'ramonb44',
+      firstname: 'ramon',
+      lastname: 'aguado',
+      email: 'aguado.soft@gmail.com',
+      password: '#NewPass',
+      phone: '918285752'
+    }).then(response => {
+      expect(response.status).to.equal(200);
+    });
+  });
+
+  it('should delete user by username', () => {
+    cy.request('DELETE', '/v2/user/ramonb44').then(response => {
+      expect(response.status).to.equal(200);
+    });
+  });
+});
